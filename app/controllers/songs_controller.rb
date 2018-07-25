b725222b005093d51c143b0f0c9a69313257677c
Brennan Fulmer <brennan.flmr@gmail.com>
2018-07-25 17:20:29 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,7 +5,7 @@ class SongsController < ApplicationController
   get "/songs" do
 =begin
     binding.pry
-    LibraryParser.parse
+    files_array = LibraryParser.parse
 =end
     @songs = Song.all
     erb :"/songs/index"
